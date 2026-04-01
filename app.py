5b4ef2a64785c16ddd80858dc2a6ee77f7f285cd
Movieeesss <114383509+Movieeesss@users.noreply.github.com>
2026-04-01 16:48:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from flask import Flask
-import movies
+import bot  # Steel scraper
+import movies # Movie scraper
+import threading
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Trichy Movie Bot Running!"
+    return "Uniq Designs Bot Server is Live!"
 
-@app.route('/run')
-def run():
-    try:
-        movies.run_all()
-        return "✅ Movie bot executed"
-    except Exception as e:
-        return f"❌ Error: {str(e)}"
+@app.route('/run-steel')
+def trigger_steel():
+    # Running in thread to avoid Render timeout
+    threading.Thread(target=bot.send_update).start()
+    return "Steel Update Triggered!"
+
+@app.route('/run-movies')
+def trigger_movies():
+    threading.Thread(target=movies.run_all).start()
+    return "Movie Update Triggered!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
