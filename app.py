7ea575c6649e0d064d794e35e1abecb976ebc9e6
Movieeesss <114383509+Movieeesss@users.noreply.github.com>
2026-04-01 16:10:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from flask import Flask
 import movies
+import traceback
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Trichy Movie Bot Running!"
+    return "✅ App Running"
 
 @app.route('/run')
 def run():
     try:
+        print("🔥 RUN TRIGGERED")
         movies.run_all()
-        return "✅ Movie bot executed"
+        return "✅ SUCCESS"
     except Exception as e:
-        return f"❌ Error: {str(e)}"
+        print("❌ ERROR:", str(e))
+        print(traceback.format_exc())
+        return f"ERROR: {str(e)}"
 
 if __name__ == "__main__":
     app.run()
