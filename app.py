3a6e903291484acf6e9e7b73a37b406fe289bb11
Movieeesss <114383509+Movieeesss@users.noreply.github.com>
2026-04-02 00:45:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
+from flask import Flask
+import threading
+import os
+import movies  # Direct-ah import pannunga
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Service is Running", 200
+
 @app.route('/run-movies')
 def trigger_movies():
-    if movies:
-        threading.Thread(target=movies.run_all).start()
-        return "OK", 200
-    return "Error", 404
+    # Threading use panna thaan cron-job timeout aagaama background-la run aagum
+    threading.Thread(target=movies.run_all).start()
+    return "OK - Scraper Started", 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
