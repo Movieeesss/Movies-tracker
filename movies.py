d177dc13ddf6a884cf8ff5c16156c1916d4c13ee
Movieeesss <114383509+Movieeesss@users.noreply.github.com>
2026-04-01 16:00:55 +0530

    Update movies.py

--- movies.py
+++ movies.py
@@ -1,4 +1,4 @@
 import os
-TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-API_KEY = os.getenv("API_KEY")
+TOKEN = os.getenv("8745585993:AAE2zRpimM9_VW9YK0I7FhDmvHb7iy1tw9A")
+CHAT_ID = os.getenv("1115358053")
+API_KEY = os.getenv("1c52b530-7d6e-4a64-b061-85cc76e6e937")
